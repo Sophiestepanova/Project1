2383da3d44bfe745e86e9cf3c3eab6aa8c6533bf
Sophiestepanova <sofi.stepanova@gmail.com>
2016-11-23 10:47:21 +0200

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random b = new Random();
+            int t = b.Next(-12, 12);
+            double k = t * b.NextDouble();
+            Console.WriteLine("{0} {1}", t, k);
+            Console.Read();
+        }
+    }
+}
