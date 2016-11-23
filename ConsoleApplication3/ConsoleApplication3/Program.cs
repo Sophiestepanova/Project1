8e9e8508b059bb484a57eb39556761fbc1d064da
Sophiestepanova <sofi.stepanova@gmail.com>
2016-11-23 11:10:33 +0200

    Add files via upload

---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace tsett
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write("По-возрастанию:");
+            int buff;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size - 1; j++)
+                {
+                    if (array[j] >= array[j + 1])
+                    {
+                        buff = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = buff;
+                    }
+                }
+            }
+            foreach (int i in array)
+            {
+                Console.Write("{0} ", i);
+            }
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size - 1; j++)
+                {
+                    if (array[j] <= array[j + 1])
+                    {
+                        buff = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = buff;
+                    }
+                }
+            }
+            Console.WriteLine("");
+            Console.Write("По-убыванию:");
+            foreach (int i in array)
+            {
+                Console.Write("{0} ", i);
+                
+            }
+            Console.Read();
+        }
+    }
+}
