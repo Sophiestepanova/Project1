13f7f5d738158ed80066c1589151aa875f31a979
Sophiestepanova <sofi.stepanova@gmail.com>
2016-11-29 21:38:21 +0200

    Add files via upload

---
+++ ДОМАШНЕЕ ЗАДАНИЕ/ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+
+            int buf;
+            int k = int.Parse(Console.ReadLine());
+            string[] array = new string[k];
+            for (int i = 0; i < k; i++)
+            {
+                array[i] = Console.ReadLine();
+            }
+            Console.Write("Массив до замены: ");
+            for (int i = 0; i < k; i++)
+            {
+                Console.Write("{0}", array[i]);
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (int.TryParse(array[i], out buf) == true)
+                {
+                    array[i] = "$";
+                }
+            }
+            Console.Write("\r\nМассив после замены: ");
+            for (int i = 0; i < k; i++)
+            {
+                Console.Write("{0}", array[i]);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ ДОМАШНЕЕ ЗАДАНИЕ/ConsoleApplication4/ConsoleApplication4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string a;
+            int buf;
+            int ch = 0;
+            a = Console.ReadLine();
+            char[] b = a.ToCharArray();
+            foreach (char l in b)
+            {
+                if (int.TryParse(Convert.ToString(l), out buf) == true)
+                {
+                    ch++;
+                }
+            }
+            Console.WriteLine("Цифр :{0}", ch);
+            Console.Read();
+        }
+
+    }
+
+}
